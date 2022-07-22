eed953fdc3068b6942e6cd585af609c2e69f6a70
Chris <65946592+idealistworld@users.noreply.github.com>
2022-07-21 22:59:22 -0700

    Add files via upload

---
+++ rsc/main.js
@@ -0,0 +1,42 @@
+const startups = [["1", "https://lalabox.io/"], ["2", "https://hulio.app/"], ["3", "https://clic.so/qcp"], ["4", "https://clic.so/itslexismith"], ["5", "https://breakroom.show/"]];
+const length = startups.length;
+var index = 0;
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    adjust();
+}, false);
+
+function adjust() {
+    if (screen.width < 1001) {
+        var source = "rsc/img/startups/" + startups[index][0] + ".svg";
+    }
+
+    if (screen.width > 1000) {
+        var source = "rsc/img/startups2/" + startups[index][0] + ".svg";
+    }
+    document.getElementById("card").src = source;
+    document.getElementById("link").setAttribute("href", startups[index][1]);
+}
+
+function next() {
+    if (index !== length - 1) {
+        index++;
+        adjust();
+    }
+    else {
+        index = 0;
+        adjust();
+    }
+}
+
+function previous() {
+    if (index === 0) {
+        index = length - 1;
+        adjust();
+    }
+    else {
+        index--;
+        adjust();
+    }
+}
